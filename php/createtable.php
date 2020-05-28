e6feb902c947f5afc1ce31cbf707753772fc88b7
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-28 17:54:44 +0800

    create table with debug with echo

--- php/createtable.php
+++ php/createtable.php
@@ -1,9 +1,13 @@
 <?php
 
     include "connection.php";
-
+ 
+    $TABLE_NAME = $_POST["table_name"];
+    echo  $_POST["table_name"]."<br>";
+    echo "apa kehe? <br>";
+        
     $sql =<<<EOF
-    CREATE TABLE HADITH (
+    CREATE TABLE $TABLE_NAME (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         hadith_no INT NOT NULL,
         page INT NOT NULL,
@@ -11,11 +15,26 @@
     )
     EOF;
 
-    $ret  = $db->exec($sql);
-    if(!$ret){
-        echo $db->lastErrorMsg();
-    }else{
-        echo "Table created successfully<br>";
+    echo "sql: ".$sql."<br>";
+
+    function create_table($sql, $db){
+
+        echo "execute create_table()"."<br>";
+        echo "sql: ".$sql."<br>";
+
+        $ret  = $db->exec($sql);
+
+        if(!$ret){
+            echo $db->lastErrorMsg();
+        }else{
+            echo "Table $TABLE_NAME created successfully<br>";
+        }
+
     }
 
-    header("Location: ../index.html");
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        echo "post received<br>";
+        create_table($sql, $db);
+    }
+
+    // header("Location: ../read.html");
